ba763b970610902aaf9240de3a2ad3490f88a7f3
ProtonGamerHun1238 <68368558+ProtonGamerHun1238@users.noreply.github.com>
2020-07-17 08:50:15 +0200

    vbjhbzbz8jzhnotnfi8

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    a_pont += 1
+    basic.showNumber(a_pont)
+    basic.pause(200)
+    basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    b_pont += 1
+    basic.showNumber(b_pont)
+    basic.pause(200)
+    basic.clearScreen()
+})
+let b_pont = 0
+let a_pont = 0
+a_pont = 0
+b_pont = 0
